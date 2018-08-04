2e389b8bb1283e30b52669c2b099f45b28c57e27
Joey Buczek <joeybuczek@gmail.com>
2018-08-04 17:41:03 -0400

    adjusting build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: "production",
-    entry: path.resolve(__dirname, "src/index.js"),
+    entry: "./src/index.js",
     output: {
       path: path.resolve(__dirname, 'lib'),
-      filename: 'index.js'
+      filename: 'index.js',
+      libraryTarget: 'commonjs2'
     },
     module: {
       rules: [
         {
-          test: /\.(js|mjs)$/,
+          test: /\.js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
@@ -27,8 +26,8 @@ module.exports = () => {
         }
       ]
     },
-    optimization: {
-      minimizer: [new UglifyJsPlugin()]
+    externals: {
+      'react': 'commonjs react' 
     }
   }
 };
